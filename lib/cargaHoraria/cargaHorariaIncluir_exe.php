3759002bb4703f7e5c5646ec36e9646f9ddddae1
Eduardo Cidral Cordeiro <ecidralcordeiro@gmail.com>
2022-06-11 23:26:05 -0300

    finalizando

--- lib/cargaHoraria/cargaHorariaIncluir_exe.php
+++ lib/cargaHoraria/cargaHorariaIncluir_exe.php
@@ -1,16 +1,15 @@
-<!DOCTYPE html>
 <html lang="pt-BR">
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Punch The Clock</title>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="../../../bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./css.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://kit.fontawesome.com/3717b64e79.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="../css/Style.css">
+<link rel="stylesheet" href="../css/CssDebug.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://kit.fontawesome.com/3717b64e79.js" crossorigin="anonymous"></script>
 
 <body>
 <?php require '../models/header.php'; ?>
